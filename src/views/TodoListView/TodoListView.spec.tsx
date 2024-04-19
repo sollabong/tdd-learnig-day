582b33ddc7d8db191dea7c5e2cd7e5a8a126fc39
Gábor Soltész-Kéri <solteszkerigabor@outlook.hu>
2024-04-19 11:44:10 +0200

    new tests added

--- src/views/TodoListView/TodoListView.spec.tsx
+++ src/views/TodoListView/TodoListView.spec.tsx
@@ -1,6 +1,7 @@
 
+import React from 'react';
 import {TodoListView} from './index'
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 // describe("<TodoListView />", () => {
 //     it("should render view", () => {
 //         throw Error("Not implemented");
@@ -13,4 +14,29 @@ describe("<TodoListView />", () => {
       expect(screen.getByTestId("todo-list-view")).toMatchSnapshot();
       expect(screen.getByTestId("todo-list-view")).toHaveTextContent("Todo List View");
     });
-  });
+    it("should render todo input field", () => {
+      render(<TodoListView />);
+      expect(screen.getByTestId("todo-input")).toBeInTheDocument();
+    });
+    it("should render button", () => {
+      render(<TodoListView />);
+      expect(screen.getByTestId("submit-button")).toBeInTheDocument();
+    });
+    it("should render list", () => {
+      render(<TodoListView />);
+      expect(screen.getByTestId("todo-list")).toBeInTheDocument();
+    });
+
+    describe("the Submit Button", () => {
+        it("should update todos state on click", () => {
+        });
+        // it("add a new element to the todos state", () => {
+        //     const { todos } = TodoListView();
+        //     // const todosLength = todos.length;
+        //     render(<TodoListView />);
+        //     const addButton = screen.getByTestId("submit-button");
+        //     fireEvent.click(addButton);
+        //     expect(todos).toContain()
+        //   });
+    })
+});
--- src/views/TodoListView/index.tsx
+++ src/views/TodoListView/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import { useState } from "react";
 
 export const TodoListView = () => {
+    const [todos, setTodos] = useState<string[]>([])
     return (
         <div data-testid="todo-list-view">
           <h1>Todo List View</h1>
+          <input data-testid="todo-input" type ="text" />
+          <button data-testid="submit-button">Submit</button>
+          <div data-testid="todo-list"></div>
         </div>
-      );
+    );
 }
